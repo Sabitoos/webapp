5f8f846b71d49c80e6c01b87eaf0b085328fd866
Sabitoos <levberlizev@gmail.com>
2025-02-16 01:34:20 +0300

    1

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -1,5 +1,4 @@
-
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 load_dotenv()
 
 from pathlib import Path
@@ -12,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DEBUG") == "False":
