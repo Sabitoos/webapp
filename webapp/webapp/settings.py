bdd950c845d4f27ac4c0d147b2effb8a7d3dee11
Sabitoos <levberlizev@gmail.com>
2025-02-16 22:59:32 +0300

    1

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = "/home/n3v0k/webapp/webapp"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
