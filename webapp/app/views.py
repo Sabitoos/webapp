14a8b43bd8e3f474c68d13a6e345b738eb40e6da
Sabitoos <levberlizev@gmail.com>
2025-02-21 23:26:59 +0300

    1

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -15,20 +15,19 @@ import json
 def korpus_view(request):
     return render(request, 'app/korpus.html')
 
-@csrf_exempt  # Отключаем CSRF-защиту для этого представления
+@csrf_exempt
 def index(request):
     if request.method == 'POST':
         try:
-            # Получаем initData из тела запроса
             data = json.loads(request.body)
             init_data = data.get('initData')
             
             if init_data:
-                # Проверяем подлинность initData
-                if validate_init_data(init_data, '7561904095:AAFmcA68_Y7vgOjiPnjXiKxUR28idE6_pCM'): 
-                    # Парсим initData для получения user_id
+                if validate_init_data(init_data, '7561904095:AAFmcA68_Y7vgOjiPnjXiKxUR28idE6_pCM'):
                     user_id = parse_init_data(init_data)
-                    return JsonResponse({'user_id': user_id})
+                    response = JsonResponse({'user_id': user_id})
+                    response['Access-Control-Allow-Origin'] = '*'  # Разрешаем CORS
+                    return response
                 else:
                     return JsonResponse({'error': 'Invalid initData'}, status=400)
             else:
@@ -38,9 +37,6 @@ def index(request):
     return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 def parse_init_data(init_data):
-    """
-    Парсим initData для извлечения user_id.
-    """
     parsed_data = parse_qs(init_data)
     user = parsed_data.get('user', [None])[0]
     if user:
@@ -49,26 +45,19 @@ def parse_init_data(init_data):
     return None
 
 def validate_init_data(init_data, bot_token):
-    """
-    Проверяет подлинность initData с использованием HMAC.
-    """
     try:
-        # Разделяем данные на пары ключ-значение
         pairs = init_data.split('&')
         data = {}
         for pair in pairs:
             key, value = pair.split('=')
             data[key] = value
 
-        # Получаем хэш и данные
         received_hash = data.pop('hash')
         data_string = '\n'.join(f"{k}={v}" for k, v in sorted(data.items()))
 
-        # Вычисляем HMAC
         secret_key = hashlib.sha256(bot_token.encode()).digest()
         computed_hash = hmac.new(secret_key, data_string.encode(), hashlib.sha256).hexdigest()
 
-        # Сравниваем хэши
         return hmac.compare_digest(computed_hash, received_hash)
     except Exception as e:
         print(f"Error validating initData: {e}")
