f725f6456df2fa4479894e147cd38b0a9ad04904
Sabitoos <levberlizev@gmail.com>
2025-02-21 23:12:47 +0300

    1

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,33 +1,42 @@
-# your_app_name/views.py
 from rest_framework import generics
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Student, Interest, Like
 from urllib.parse import parse_qs
 from .serializers import StudentSerializer, InterestSerializer, LikeSerializer
 from django.shortcuts import render
 import hmac
 import hashlib
+import json
 
 #def index(request):
 #    return render(request, 'app/fio.html')
 def korpus_view(request):
     return render(request, 'app/korpus.html')
 
+@csrf_exempt  # Отключаем CSRF-защиту для этого представления
 def index(request):
-    # Получаем initData из запроса
-    init_data = request.GET.get('initData')
-    
-    if init_data:
-        # Проверяем подлинность initData
-        if validate_init_data(init_data, '7561904095:AAFmcA68_Y7vgOjiPnjXiKxUR28idE6_pCM'):  # Замените на токен вашего бота
-            # Парсим initData для получения user_id
-            user_id = parse_init_data(init_data)
-            return render(request, 'app/index.html', {'user_id': user_id})
-        else:
-            return HttpResponse("Invalid initData.")
-    else:
-        return HttpResponse("Init data not provided.")
-    
+    if request.method == 'POST':
+        try:
+            # Получаем initData из тела запроса
+            data = json.loads(request.body)
+            init_data = data.get('initData')
+            
+            if init_data:
+                # Проверяем подлинность initData
+                if validate_init_data(init_data, '7561904095:AAFmcA68_Y7vgOjiPnjXiKxUR28idE6_pCM'):
+                    # Парсим initData для получения user_id
+                    user_id = parse_init_data(init_data)
+                    return JsonResponse({'user_id': user_id})
+                else:
+                    return JsonResponse({'error': 'Invalid initData'}, status=400)
+            else:
+                return JsonResponse({'error': 'initData not provided'}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 def parse_init_data(init_data):
     """
     Парсим initData для извлечения user_id.
