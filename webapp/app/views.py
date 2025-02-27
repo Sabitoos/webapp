9aa558b5c4ab3ce94ace8cd7672048a3a27f177b
Sabitoos <levberlizev@gmail.com>
2025-02-27 17:54:36 +0300

    1

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -17,8 +17,23 @@ def index(request):
 
     return HttpResponse('<h1>Ты подключился!</h1>')
 
-def korpus_view(request):
-    return render(request, 'app/korpus.html')
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+    name = ''
+    if message.from_user.last_name is None:
+        name = f'{message.from_user.first_name}'
+    else:
+        name = f'{message.from_user.first_name} {message.from_user.last_name}'
+    bot.send_message(message.chat.id, f'Привет! {name}\n'
+                                      f'Я бот, который будет спамить вам беседу :)\n\n'
+                                      f'Чтобы узнать больше команд, напишите /help')
+    
+    
+# Webhook
+bot.set_webhook(url="https://myapp.pythonanywhere.com/" + os.getenv('BOT_KEY'))
+
+#def korpus_view(request):
+#    return render(request, 'app/korpus.html')
 
 class InterestListCreate(generics.ListCreateAPIView):
     queryset = Interest.objects.all()
