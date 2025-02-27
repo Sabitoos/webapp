a66289dad1490661f54b2303b672e4f80506255f
Sabitoos <levberlizev@gmail.com>
2025-02-27 15:09:00 +0300

    manage.py botstart

---
+++ webapp/app/management/commands/botstart.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from dotenv import load_dotenv
+load_dotenv()
+
+from telebot import TeleBot
+
+
+# Объявление переменной бота
+bot = TeleBot(settings.os.getenv('BOT_KEY'), threaded=False)
+
+
+# Название класса обязательно - "Command"
+class Command(BaseCommand):
+  	# Используется как описание команды обычно
+    help = 'Just a command for launching a Telegram bot.'
+
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        your_variable = message.from_user.id
+
+    
